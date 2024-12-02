2b23bac3b687377b0a54b76c46b996e96030db82
Artem <sidoruck.ar@yandex.ru>
2024-12-03 04:52:53 +0800

    cart

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -9,3 +9,17 @@ exports.findAll = async (req, res) => {
         res.status(500).send('Server Error');
     }
 };
+
+exports.findById = async (req, res) => {
+    const genreId = req.params.id; // Получаем ID жанра из параметров запроса
+    try {
+        const genre = await Genre.findByPk(genreId); // Находим жанр по ID
+        if (!genre) {
+            return res.status(404).send('Genre not found'); // Если жанр не найден, возвращаем 404
+        }
+        res.json(genre.name); // Возвращаем найденный жанр
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+};
--- routes/genre.route.js
+++ routes/genre.route.js
@@ -2,6 +2,8 @@ const authMiddleware = require('../middleware/authMiddle.js');
 module.exports = (app) => {
     const GenresController = require('../controllers/genre.controller'); 
 
-    app.get('/api/genres',authMiddleware, GenresController.findAll); 
+    app.get('/api/genres',authMiddleware, GenresController.findAll);
+    
+    app.get('/api/genres/:id',authMiddleware, GenresController.findById); 
 
 };
